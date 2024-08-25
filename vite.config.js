7c4f217f6bca3149f44b76f07c83834cf0652201
Negus <natnaelbirhanu22@gmail.com>
2024-08-25 20:03:17 +0300

    vite configured for docker

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,16 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        watch: {
+          usePolling: true,
+        },
+        hmr: {
+          host: 'localhost',
+        },
+      },
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
