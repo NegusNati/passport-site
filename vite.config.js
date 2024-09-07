0cbdcf638670640c9cbe10b616fe7b74f69b397f
Negus <natnaelbirhanu22@gmail.com>
2024-09-07 15:25:05 +0300

    ready for prod

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,18 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        watch: {
-          usePolling: true,
-        },
-        hmr: {
-          host: 'localhost',
-        },
-      },
-    plugins: [
+      plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input:  [
+                'resources/css/app.css',
+                'resources/js/app.jsx',
+            ],
             refresh: true,
         }),
         react(),
     ],
+    build: {
+        outDir: 'public/build', // Output directory for built files
+        manifest: true, // Ensure manifest is generated
+    },
 });
